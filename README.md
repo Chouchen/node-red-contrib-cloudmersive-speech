88528c575cec4d62044bd93b937edf77fa510bb7
Clement Desmidt <clement@desmidt.fr>
2022-02-23 16:38:56 +0100

    :tada: Hello world

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cloudmersive speech for NodeRED
+
+## Installation
+
+`npm install node-red-contrib-cloudmersive-speech`
+
+## Quick Start
+
+You will need valid Cloudmersive developer credentials in the form of a set of consumer and access tokens/keys.  You can get these [here](https://account.cloudmersive.com/signup).
+
+
---
+++ cloudmersive-speech.js
@@ -0,0 +1,59 @@
+var CloudmersiveSpeechApiClient = require('cloudmersive-speech-api-client');
+var defaultClient = CloudmersiveSpeechApiClient.ApiClient.instance;
+var fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+
+
+module.exports = function(RED) {
+    'use strict';
+
+    function Text2Speech(n) {
+      
+        RED.nodes.createNode(this,n);
+        this.path = n.path;
+        this.format = n.format;
+
+        var node = this;
+        
+        node.on('input', function (msg) {
+            
+          const creds = RED.nodes.getNode(n.creds);
+            // Configure API key authorization: Apikey
+            var Apikey = defaultClient.authentications['Apikey'];
+            Apikey.apiKey = creds.apiKey;
+
+            var api = new CloudmersiveSpeechApiClient.SpeakApi()
+
+            var reqConfig = new CloudmersiveSpeechApiClient.TextToSpeechRequest();
+            reqConfig.Format = node.format || "mp3";
+            reqConfig.Text = msg.payload;
+
+            var callback = function(error, data, response) {
+                if (error) {
+                  console.error(error);
+                } else {
+                  const folder = node.path || "./";
+                  const filename = folder+uuidv4()+"."+node.format;
+                  fs.writeFile(filename, data, function(err) {
+                      if (err) {
+                        console.error(err);
+                        throw err;
+                      }
+                      fs.realpath(filename, (error, resolvedPath) => {
+                        if (error) {
+                          console.log(error);
+                        }
+                        else {
+                          node.send({payload: resolvedPath});
+                        }
+                      });
+                  });
+                }
+              };
+              
+              api.speakTextToSpeech(reqConfig, callback);
+        });
+    }
+
+    RED.nodes.registerType('cloudmersive-speech', Text2Speech);
+};
